485c49cfab40ffb9b04d0ab6df0d03728e8fa4c8
Gulzat33 <islamainazik2007@gmail.com>
2024-04-17 10:43:28 +0600

    первая фиксация

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
-function App() {
+const App = () => {
+    const counter = useSelector((state) => state.count)
+    const dispatch = useDispatch()
+
+
+    const increment = () => {
+        dispatch({type:'INCREMENT'})
+    }
+    const decrement = () => {
+        dispatch({type:'DECREMENT'})
+    }
+
+    const addTen = () => {
+        dispatch({type:'ADD-TEN'})
+    }
+    const clear = () => {
+        dispatch({type:'CLEAR'})
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+            <span>{counter}</span>
+          <button onClick={increment}>+</button>
+          <button onClick={decrement}>-</button>
+          <button onClick={addTen}>+10</button>
+          <button onClick={clear}>clear</button>
+      </div>
   );
-}
+};
 
 export default App;
